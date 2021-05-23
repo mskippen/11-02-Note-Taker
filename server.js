95e1cb2e9b895d7f5fd905dcb499da7b26fff24e
Megan Skippen <79444992+mskippen@users.noreply.github.com>
2021-05-23 20:19:59 +1000

    all coding files created

---
+++ server.js
@@ -0,0 +1,55 @@
+const bodyParser = require("body-parser");
+const express = require("express")
+const path = require("path")
+const { v4: uuidv4 } = require('uuid');
+const fs = require("fs")
+
+const app = express()
+app.use(express.json())
+app.use(express.static("public"))
+
+const data = fs.readFileSync("./db/db.json", (err) => {
+    if(err) throw err;
+})
+const parsedData = JSON.parse(data)
+
+//HTML ROUTES/
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notes.html"))
+})
+
+// API ROUTES
+app.get("/api/notes", (rez, res) => {
+    res.sendFile(path.join(__dirname, "db", "db.json"))
+})
+
+app.post("/api/notes", (req, res) => {
+    const requestBody = {
+        id: uuidv4(),
+        title: req.body.title,
+        text: req.body.text
+    }
+    parsedData.push(requestBody)
+    console.log(parsedData)
+    fs.writeFile("./db/db.json", JSON.stringify(parsedData), (err) => {
+        if(err) throw err;
+    })
+    res.json(parsedData)
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    const filteredData = parsedData.filter(item => item.id != req.params.id)
+    fs.writeFile("./db/db.json", JSON.stringify(filteredData), (err) => {
+        if(err) throw err;
+    })
+    console.log(filteredData)
+    res.json(filteredData)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log( `server running on port ${PORT}` ))
